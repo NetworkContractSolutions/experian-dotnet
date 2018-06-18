29d53658eb4840afeed8fe20f15b397a2aa3bed6
mike.woods <mike.woods@ncontracts.com>
2018-06-18 16:02:58 -0500

    made amount nullable

--- src/Client/Bis/TotalAccountBalanceResult.cs
+++ src/Client/Bis/TotalAccountBalanceResult.cs
@@ -2,7 +2,7 @@ namespace Experian.Api.Client.Bis
 {
     public sealed class TotalAccountBalanceResult
     {
-        public int Amount { get; set; }
+        public int? Amount { get; set; }
 
         public string Modifier { get; set; }
     }
